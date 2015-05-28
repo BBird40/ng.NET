3af5fee581a65fe9d626a672f4cd058738d06f53
BByrd <bryanjebyrd@gmail.com>
2015-05-27 22:34:26 -0500

    renamed ng.NET to ng.NET.Server.

--- ng.NET/ng.NET/Controllers/EmployeeController.cs
+++ ng.NET/ng.NET/Controllers/EmployeeController.cs
@@ -1,12 +1,8 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using DomainModel;
 
 namespace ng.NET.Controllers
 {
