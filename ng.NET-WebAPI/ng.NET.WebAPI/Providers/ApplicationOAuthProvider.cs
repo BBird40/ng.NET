901984c513ad2ce2621902e2cf70835d5d89c92a
BByrd <bryanjebyrd@gmail.com>
2015-07-19 17:12:34 -0500

    fixed Access Control Allow Origin issue in chrome.

--- ng.NET-WebAPI/ng.NET.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ ng.NET-WebAPI/ng.NET.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -26,10 +26,9 @@ namespace ng.NET.WebAPI.Providers
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
-            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
             var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
-
             ApplicationUser user = await userManager.FindAsync(context.UserName, context.Password);
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
 
             if (user == null)
             {
