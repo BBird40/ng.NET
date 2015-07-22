3bef06751ab56fbc472925f688c4ec2576f62a03
BByrd <bryanjebyrd@gmail.com>
2015-07-22 18:44:49 -0500

    removing unused directives.

--- ng.NET-MVC/ng.NET/Global.asax.cs
+++ ng.NET-MVC/ng.NET/Global.asax.cs
@@ -1,9 +1,7 @@
 ﻿using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
